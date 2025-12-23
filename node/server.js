42054ebad27758dea4ae452c36bbb00c5f841bfb
Martha Kafri (Марфа Красносельская) <149630148+mkafri@users.noreply.github.com>
2025-12-23 20:04:24 +0300

    Testing the workflows

--- node/server.js
+++ node/server.js
@@ -1,5 +1,5 @@
 const server = require("express")();
 server.listen(3000, async () => { });
 server.get("/super-app", async (_, response) => {
-    response.json({ "super-mega": "app" });
+    response.json({ "super-mega-wow": "app" });
 });
